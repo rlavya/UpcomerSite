0e75f6d0f594c1c0bcb4c7e6c32e39ac03afa3ff
Lavya R <lavya@qburst.com>
2016-03-28 19:13:34 +0530

    done the review changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,19 @@ $(document).ready(function() {
 	        scrollTop: $("#section2").offset().top - 83
 	    }, 1500);
 	});
+	$("#signup").click(function() {
+		var email = $('#email').val();
+		$(".content1").css("display", "none");
+		$(".hidden_content1").css("display", "block");
+	});
+	$(".menu_button").click(function() {
+		$('.hidden_header').css("display", "block");
+	    var hidden_menu = document.getElementById('hidden_header');
+	    if (hidden_menu.className == "hidden_header show") {
+	        hidden_menu.className = "hidden_header hide";
+	    }
+	    else {
+	        hidden_menu.className = "hidden_header show";
+	    }
+	});
 });
-function signup() {
-	var email = $('#email').val();
-	$(".content1").css("display", "none");
-	$(".hidden_content1").css("display", "block");
-}
-function show_menu() {
-	$('.hidden_header').css("display", "block");
-    var hidden_menu = document.getElementById('hidden_header');
-    if (hidden_menu.className == "hidden_header show") {
-        hidden_menu.className = "hidden_header hide";
-    }
-    else {
-        hidden_menu.className = "hidden_header show";
-    }
-}
